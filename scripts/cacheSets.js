66becce85765db9d29ccc804bacd41fb28e48929
Benjamin Lannon <benjamin@lannonbr.com>
2024-07-21 11:07:43 -0400

    Add cache sets script. Closes #5

---
+++ scripts/cacheSets.js
@@ -0,0 +1,45 @@
+const dataGitUrl = "https://github.com/PokemonTCG/pokemon-tcg-data";
+
+const { execSync } = require("child_process");
+
+execSync(`git clone ${dataGitUrl}`);
+
+const sets = require("../pokemon-tcg-data/sets/en.json");
+
+const fs = require("fs");
+
+fs.writeFileSync(
+  "./data/sets.json",
+  JSON.stringify(
+    sets.map((set) => {
+      return {
+        name: set.name,
+        description: `${set.id} (released ${set.releaseDate})`,
+        value: set.id,
+      };
+    })
+  )
+);
+
+const setsPaths = fs.readdirSync("./pokemon-tcg-data/cards/en/");
+
+fs.mkdirSync("./data/sets");
+
+for (let setPath of setsPaths) {
+  const set = require(`../pokemon-tcg-data/cards/en/${setPath}`);
+  const setLen = set.length;
+
+  fs.writeFileSync(
+    `./data/sets/${setPath}`,
+    JSON.stringify(
+      set.map((card) => {
+        return {
+          name: `${card.name} (${card.number}/${setLen})`,
+          value: card.id,
+        };
+      })
+    )
+  );
+}
+
+execSync("rm -rf pokemon-tcg-data");
